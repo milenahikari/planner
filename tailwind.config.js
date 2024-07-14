f6246354dcb1f242e3af45e628089ef98f658f86
Milena <milena@wapstore.com.br>
2024-07-14 17:42:32 -0300

    feat: add theme configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./App.{ts,tsx}", "./app/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xs: '12px',
+        sm: '14px',
+        md: '16px',
+        lg: '24px',
+        xl: '44px',
+      },
+      colors: {
+        gray: {
+          50: '#FFFFFF',
+          100: '#F6F6F5',
+          200: '#BCC1CD',
+          500: '#88889D',
+          900: '#212525',
+          950: '#212523',
+        },
+        orange: {
+          50: '#FF7648'
+        },
+        green: {
+          50: '#4DC591'
+        }
+      }
+    },
   },
   plugins: [],
 }
